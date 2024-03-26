fe1b94f162234218c6257fed1078ebb0886a0dc9
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-03-26 16:42:18 -0300

    app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 
 var cardapio = {};
 
+var MEU_CARRINHO = [];
+
 cardapio.eventos = {
   init: () => {
     cardapio.metodos.obterItensCardapio();
@@ -24,7 +26,8 @@ cardapio.metodos = {
       let temp = cardapio.templates.item
         .replace(/\${img}/g, e.img)
         .replace(/\${nome}/g, e.name)
-        .replace(/\${preco}/g, e.price.toFixed(2).replace(".", ","));
+        .replace(/\${preco}/g, e.price.toFixed(2).replace(".", ","))
+        .replace(/\${id}/g, e.id);
 
       //botão ver mais foi clicado  (12 itens)
       if (vermais && i >= 8 && i < 12) {
@@ -48,8 +51,61 @@ cardapio.metodos = {
   verMais: () => {
     var ativo = $(".container-menu a.active").attr("id").split("menu-")[1];
     cardapio.metodos.obterItensCardapio(ativo, true);
+  },
 
-    $("#btnVerMais").addClass("hidden");
+  //diminuir a quantidade do item na seção cardápio
+  diminuirQuantidade: (id) => {
+    let qntdAtual = parseInt($("#qntd-" + id).text());
+
+    if (qntdAtual > 0) {
+      $("#qntd-" + id).text(qntdAtual - 1);
+    }
+  },
+
+  //aumentar a quantidade do item na seção cardápio
+  aumentarQuantidade: (id) => {
+    let qntdAtual = parseInt($("#qntd-" + id).text());
+    $("#qntd-" + id).text(qntdAtual + 1);
+  },
+
+  //adicionar produto ao carrinho o item do cardápio
+  adicionarAoCarrinho: (id) => {
+    let qntdAtual = parseInt($("#qntd-" + id).text());
+
+    if (qntdAtual > 0) {
+      //obter a categoria ativa
+      var categoria = $(".container-menu a.active")
+        .attr("id")
+        .split("menu-")[1];
+
+      //obter a lista de itens
+      let filtro = MENU[categoria];
+
+      //obter o  item
+      let item = $.grep(filtro, (e, i) => {
+        return e.id == id;
+      });
+
+      if (item.length > 0) {
+        //validar se já existe esse item no carrinho
+        let existe = $.grep(MEU_CARRINHO, (elem, index) => {
+          return elem.id == id;
+        });
+
+        //caso já exista o item no carrinho só altera a quantidade
+        if (existe.length > 0) {
+          let objIndex = MEU_CARRINHO.findIndex((obj) => obj.id == id);
+          MEU_CARRINHO[objIndex].qntd = MEU_CARRINHO[objIndex].qntd + qntdAtual;
+        }
+        //caso não exista o item no carrinho adicio-na ele
+        else {
+          item[0].qntd = qntdAtual;
+          MEU_CARRINHO.push(item[0]);
+        }
+
+        $("#qntd-" + id).text(0);
+      }
+    }
   },
 };
 
@@ -57,7 +113,7 @@ cardapio.templates = {
   item: `
 
       <div class="col-3 mb-5">
-                <div class="card card-item">
+                <div class="card card-item" id="\${id}">
                   <div class="img-produto">
                     <img
                       src="\${img}"/>
@@ -67,10 +123,10 @@ cardapio.templates = {
                   </p>
                   <p class="price-produto text-center"><b>R$ \${preco}</b></p>
                   <div class="add-carrinho">
-                    <span class="btn-menos"><i class="fas fa-minus"></i></span>
-                    <span class="add-numero-itens">0</span>
-                    <span class="btn-mais"><i class="fas fa-plus"></i></span>
-                    <span class="btn btn-add"
+                    <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
+                    <span class="add-numero-itens" id="qntd-\${id}">0</span>
+                    <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
+                    <span class="btn btn-add" onclik="cardapio.metodos.adicionarAoCarrinho('\${id}')"
                       ><i class="fa fa-shopping-bag"></i
                     ></span>
                   </div>
