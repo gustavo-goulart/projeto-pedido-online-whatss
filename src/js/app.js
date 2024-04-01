bafcd169da8a63c4dc0f9e3e471619d43cd23096
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-04-01 12:11:49 -0300

    add.js

--- src/js/app.js
+++ src/js/app.js
@@ -5,15 +5,23 @@ $(document).ready(function () {
 var cardapio = {};
 
 var MEU_CARRINHO = [];
+var MEU_ENDERECO = null;
+
+var VALOR_CARRINHO = 0;
+var VALOR_ENTREGA = 7.5;
+
+var CELULAR_EMPRESA = "5541991229884";
 
 cardapio.eventos = {
   init: () => {
     cardapio.metodos.obterItensCardapio();
+    cardapio.metodos.carregarBotaoLigar();
+    cardapio.metodos.carregarBotaoReserva();
   },
 };
 
 cardapio.metodos = {
-  //obtem a lista de itens do cardápio
+  // obtem a lista de itens do cardápio
   obterItensCardapio: (categoria = "burgers", vermais = false) => {
     var filtro = MENU[categoria];
 
@@ -29,31 +37,33 @@ cardapio.metodos = {
         .replace(/\${preco}/g, e.price.toFixed(2).replace(".", ","))
         .replace(/\${id}/g, e.id);
 
-      //botão ver mais foi clicado  (12 itens)
+      // botão ver mais foi clicado (12 itens)
       if (vermais && i >= 8 && i < 12) {
         $("#itensCardapio").append(temp);
       }
 
-      //paginação inicial (8itens)
+      // paginação inicial (8 itens)
       if (!vermais && i < 8) {
         $("#itensCardapio").append(temp);
       }
     });
 
-    //remover classe active do botão burguer
+    // remove o ativo
     $(".container-menu a").removeClass("active");
 
-    //seta o menu para ativo
+    // seta o menu para ativo
     $("#menu-" + categoria).addClass("active");
   },
 
-  //clique no botão de ver mais
+  // clique no botão de ver mais
   verMais: () => {
     var ativo = $(".container-menu a.active").attr("id").split("menu-")[1];
     cardapio.metodos.obterItensCardapio(ativo, true);
+
+    $("#btnVerMais").addClass("hidden");
   },
 
-  //diminuir a quantidade do item na seção cardápio
+  // diminuir a quantidade do item no cardapio
   diminuirQuantidade: (id) => {
     let qntdAtual = parseInt($("#qntd-" + id).text());
 
@@ -62,13 +72,13 @@ cardapio.metodos = {
     }
   },
 
-  //aumentar a quantidade do item na seção cardápio
+  // aumentar a quantidade do item no cardapio
   aumentarQuantidade: (id) => {
     let qntdAtual = parseInt($("#qntd-" + id).text());
     $("#qntd-" + id).text(qntdAtual + 1);
   },
 
-  //adicionar produto ao carrinho o item do cardápio
+  // adicionar ao carrinho o item do cardápio
   adicionarAoCarrinho: (id) => {
     let qntdAtual = parseInt($("#qntd-" + id).text());
 
@@ -103,37 +113,73 @@ cardapio.metodos = {
           MEU_CARRINHO.push(item[0]);
         }
 
-        cardapio.metodos.mensagem("Item adicionado ao carrinho", "green");
         $("#qntd-" + id).text(0);
 
         cardapio.metodos.atualizarBadgeTotal();
+
+        cardapio.metodos.mensagem("Item adicionado ao carrinho", "green");
       }
     }
+  },
+
+  // atualiza o botões totais do carrinho de compras
+  atualizarBadgeTotal: () => {
+    var total = 0;
+
+    $.each(MEU_CARRINHO, (i, e) => {
+      total += e.qntd;
+    });
+
+    if (total > 0) {
+      $(".botao-carrinho").removeClass("hidden");
+      $(".container-total-carrinho").removeClass("hidden");
+    } else {
+      $(".botao-carrinho").addClass("hidden");
+      $(".container-total-carrinho").addClass("hidden");
+    }
+
+    $(".badge-total-carrinho").html(total);
+  },
+
+  //mensagem padrão de alertas na página
+  mensagem: (texto, cor = "red", tempo = 2000) => {
+    //remover mensagem da tela
+    let id = Math.floor(Date.now() * Math.random()).toString();
+
+    let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`;
+
+    $("#container-mensagens").append(msg);
+
+    //tempo para executar a função
+    setTimeout(() => {
+      //remover mensagem de alerta na página
+      $("#msg-" + id).removeClass("fadeInDown");
+      $("#msg-" + id).addClass("fadeOutUp");
+      $("#msg-" + id).remove();
+    }, tempo);
   },
 };
 
 cardapio.templates = {
   item: `
-
-      <div class="col-3 mb-5">
-                <div class="card card-item" id="\${id}">
-                  <div class="img-produto">
-                    <img
-                      src="\${img}"/>
-                  </div>
-                  <p class="title-produto text-center mt-4">
+        <div class="col-12 col-lg-3 col-md-3 col-sm-6 mb-5 animated fadeInUp">
+            <div class="card card-item" id="\${id}">
+                <div class="img-produto">
+                    <img src="\${img}" />
+                </div>
+                <p class="title-produto text-center mt-4">
                     <b>\${nome}</b>
-                  </p>
-                  <p class="price-produto text-center"><b>R$ \${preco}</b></p>
-                  <div class="add-carrinho">
+                </p>
+                <p class="price-produto text-center">
+                    <b>R$ \${preco}</b>
+                </p>
+                <div class="add-carrinho">
                     <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
                     <span class="add-numero-itens" id="qntd-\${id}">0</span>
                     <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
-                    <span class="btn btn-add" onclik="cardapio.metodos.adicionarAoCarrinho('\${id}')"
-                      ><i class="fa fa-shopping-bag"></i
-                    ></span>
-                  </div>
+                    <span class="btn btn-add" onclick="cardapio.metodos.adicionarAoCarrinho('\${id}')"><i class="fa fa-shopping-bag"></i></span>
                 </div>
-              </div>
-      `,
+            </div>
+        </div>
+    `,
 };
