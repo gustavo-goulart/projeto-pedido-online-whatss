9549b1af76fba58c3c30382437bf4b2917a531db
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-04-01 14:07:44 -0300

    Manipulando Etapas do Carrinho

--- src/js/app.js
+++ src/js/app.js
@@ -145,9 +145,67 @@ cardapio.metodos = {
   abrirCarrinho: (abrir) => {
     if (abrir) {
       $("#modalCarrinho").removeClass("hidden");
+      cardapio.metodos.carregarEtapa(1);
     } else {
       $("#modalCarrinho").addClass("hidden");
     }
+  },
+
+  //alterar texto e carregar botões das etapas
+  carregarEtapa: (etapa) => {
+    if (etapa == 1) {
+      $("#lblTituloEtapa").text("Seu carrinho:");
+      $("#itensCarrinho").removeClass("hidden");
+      $("#localEntrega").addClass("hidden");
+      $("#resumoCarrinho").addClass("hidden");
+
+      $(".etapa").removeClass("active");
+      $(".etapa1").addClass("active");
+
+      $("#btnEtapaPedido").removeClass("hidden");
+      $("#btnEtapaEndereco").addClass("hidden");
+      $("#btnEtapaResumo").addClass("hidden");
+      $("#btnVoltar").addClass("hidden");
+    }
+
+    if (etapa == 2) {
+      $("#lblTituloEtapa").text("Endereço de entrega:");
+      $("#itensCarrinho").addClass("hidden");
+      $("#localEntrega").removeClass("hidden");
+      $("#resumoCarrinho").addClass("hidden");
+
+      $(".etapa").removeClass("active");
+      $(".etapa1").addClass("active");
+      $(".etapa2").addClass("active");
+
+      $("#btnEtapaPedido").addClass("hidden");
+      $("#btnEtapaEndereco").removeClass("hidden");
+      $("#btnEtapaResumo").addClass("hidden");
+      $("#btnVoltar").removeClass("hidden");
+    }
+
+    if (etapa == 3) {
+      $("#lblTituloEtapa").text("Resumo do pedido:");
+      $("#itensCarrinho").addClass("hidden");
+      $("#localEntrega").addClass("hidden");
+      $("#resumoCarrinho").removeClass("hidden");
+
+      $(".etapa").removeClass("active");
+      $(".etapa1").addClass("active");
+      $(".etapa2").addClass("active");
+      $(".etapa3").addClass("active");
+
+      $("#btnEtapaPedido").addClass("hidden");
+      $("#btnEtapaEndereco").addClass("hidden");
+      $("#btnEtapaResumo").removeClass("hidden");
+      $("#btnVoltar").removeClass("hidden");
+    }
+  },
+
+  //botão voltar etapa no carrinho de compras
+  voltarEtapa: () => {
+    let etapa = $(".etapa.active").length;
+    cardapio.metodos.carregarEtapa(etapa - 1);
   },
 
   //mensagem padrão de alertas na página
